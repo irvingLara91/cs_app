d73ade8fd6eb44fdb8cf226bb836b59b74f95b01
Mario Meraz <mario.meraz@gmail.com>
2022-01-31 15:20:14 -0600

    Settings in interface, carousel, logo
    settings in interface, carousel, logo

--- components/PostSplash/index.jsx
+++ components/PostSplash/index.jsx
@@ -1,20 +1,85 @@
-import { View, Center, Stack, Text, Box, Button} from "native-base";
+import React, { useRef, useState } from "react";
+import { Dimensions } from "react-native";
+import { View, Center, Stack, Text, Box, Button, Image } from "native-base";
 import screens from "~/constants/screens";
+import Carousel, { Pagination } from "react-native-snap-carousel";
+import styles from "./styles";
 
-const PostSplash = ({navigation}) => {
+const SLIDER_WIDTH = Dimensions.get("window").width;
+const ITEM_WIDTH = Math.round(SLIDER_WIDTH);
+const ITEM_HEIGHT = Math.round((ITEM_WIDTH * 3) / 4);
+
+const PostSplash = ({ navigation }) => {
+  const _carousel = useRef(null);
+  const [slide, setSlide] = useState(0);
+
+  const carouselItems = [
+    {
+      img: <Image alt="image" source={require("~/assets/image.png")} />,
+      title: "How Does it Work?",
+      msg: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elementum consectetur amet tellus lobortis diam sed.",
+    },
+    {
+      img: <Image alt="image" source={require("~/assets/image.png")} />,
+      title: "How Does it Work?",
+      msg: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elementum consectetur amet tellus lobortis diam sed.",
+    },
+    {
+      img: <Image alt="image" source={require("~/assets/image.png")} />,
+      title: "How Does it Work?",
+      msg: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elementum consectetur amet tellus lobortis diam sed.",
+    },
+  ];
+
+  const _renderItem = ({ item, index }) => {
+    return (
+      <Box style={styles.itemContainer}>
+        <Text>{item.img}</Text>
+        <Text fontSize="2xl" fontWeight="black" textAlign={"center"}>
+          {item.title}
+        </Text>
+        <Text>{item.msg}</Text>
+      </Box>
+    );
+  };
+
   return (
     <Center>
-      <Stack mt={3} space={4} w="75%" maxW="300px">
-        <View><Text>Carousel here</Text></View>
+      <Stack mt={50}>
+        <Image alt="image" source={require("~/assets/cornerstone-logo-250px.png")} />
+      </Stack>
+      <Stack mt={100} space={4} w="75%" maxW="300px">
+        <Carousel
+          layout={"stack"}
+          layoutCardOffset={3}
+          ref={_carousel}
+          data={carouselItems}
+          renderItem={_renderItem}
+          sliderWidth={SLIDER_WIDTH}
+          itemWidth={ITEM_WIDTH}
+          onSnapToItem={(i) => setSlide(i)}
+        />
+        <Pagination
+          containerStyle={{}}
+          dotStyle={styles.do}
+          inactiveDotOpacity={0.4}
+          inactiveDotScale={0.6}
+          activeDotIndex={slide}
+          dotsLength={carouselItems.length}
+        />
         <Box>
-          <Button onPress={() => navigation.navigate(screens.LOGIN)}>Login</Button>
+          <Button onPress={() => navigation.navigate(screens.LOGIN)}>
+            Login
+          </Button>
         </Box>
         <Box>
-          <Button onPress={() => navigation.navigate(screens.REGISTER)}>Register</Button>
+          <Button onPress={() => navigation.navigate(screens.REGISTER)}>
+            Register
+          </Button>
         </Box>
       </Stack>
     </Center>
   );
-}
+};
 
-export default PostSplash
+export default PostSplash;
---
+++ components/PostSplash/styles.js
@@ -0,0 +1,26 @@
+
+import { StyleSheet, Dimensions } from "react-native";
+
+const SLIDER_WIDTH = Dimensions.get('window').width;
+const ITEM_WIDTH = Math.round(SLIDER_WIDTH  / 1.3);
+const ITEM_HEIGHT = Math.round(ITEM_WIDTH);
+
+module.exports = StyleSheet.create({
+    dots: {
+        width: 10,
+        height: 10,
+        borderRadius: 5,
+        marginHorizontal: 8,
+        backgroundColor: "rgba(204, 51, 44, 0.92)",
+      },
+      itemContainer: {  
+        width: ITEM_WIDTH,
+        height: ITEM_HEIGHT,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#C4C4C4',
+        padding: 10,
+        
+      },
+
+})
