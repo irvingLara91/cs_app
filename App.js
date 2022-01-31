34ed72e797bd2b53f24bb536ea55f763c6540346
eovando-93 <emmanuel.ovando@si-quimera.com.mx>
2022-01-31 08:16:00 -0600

    setup absolute path, added react-navigation and native-base

--- App.js
+++ App.js
@@ -1,20 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NativeBaseProvider } from "native-base";
+
+import LoginScreen from "./components/Login";
+import HomeScreen from "./components/Home";
+
+import routes from "~/constants/routes";
 
 export default function App() {
+  const Stack = createNativeStackNavigator();
+  const { Navigator, Screen } = Stack;
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NativeBaseProvider>
+      <NavigationContainer>
+        <Navigator>
+          <Screen
+            name={routes.HOME}
+            component={HomeScreen}
+          />
+          <Screen
+            name={routes.LOGIN}
+            component={LoginScreen}
+          />
+        </Navigator>
+      </NavigationContainer>
+    </NativeBaseProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
