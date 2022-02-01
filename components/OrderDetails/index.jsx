f0939915ad88259954cbc518a8df2e7f84a2e3b2
eovando-93 <emmanuel.ovando@si-quimera.com.mx>
2022-02-01 14:21:33 -0600

    layout - OrderDetails, Orders

---
+++ components/OrderDetails/index.jsx
@@ -0,0 +1,56 @@
+import React, { useEffect, useState } from "react";
+import { Stack, Center, Divider, View, Text, Box, Heading, Image, Flex, Container } from "native-base";
+import Status from "./Status";
+import ordersService from "~/services/orders";
+import gravestoneMedia from "~/assets/gravestone-media.png";
+
+const OrderDetails = ({route}) => {
+
+	const [details, setDetails] = useState({});
+
+	useEffect(() => {
+		const getOrderDetails = async() => {
+			const result = await ordersService.getOrderDetails(route.params.orderId);
+			console.log({result});
+			setDetails(result);
+		};
+		getOrderDetails();
+	}, []);
+
+	return (
+		<Stack p="5">
+			<Box w="full" borderWidth="1" borderRadius="lg">
+				<Center>
+					<Text>Order:</Text>
+					<Text fontSize="24" fontWeight="bold">{route.params.orderId}</Text>
+					<Text >{details.date}</Text>
+				</Center>
+			</Box>
+			<Flex direction="column">
+				<Heading mt="5" mb="5">Order Details</Heading>
+				<Flex direction="row" w="full" justify="space-between" align="center" borderWidth={1} borderRadius="lg" p="5">
+					<Text fontWeight="bold">Gravestone Picture</Text>
+					<Box bgColor="muted.300" pt="5" pb="5" pl="10" pr="10" borderRadius="lg">
+						<Image source={gravestoneMedia} alt="gravestone picture" />
+					</Box>
+				</Flex>
+				<Box mt="5">
+					<Text fontWeight="bold">Gravestone text</Text>
+					<Text>{details.text}</Text>
+				</Box>
+				<Box mt="5">
+					<Text fontWeight="bold">Additional instructions</Text>
+					<Text>{details.additionalInstructions}</Text>
+				</Box>
+				<Box mt="5">
+					<Text fontWeight="bold">Gravestone address</Text>
+					<Text>{details.address}</Text>
+				</Box>
+			</Flex>
+			<Divider mt="2" />
+			<Status {...details.status} />
+		</Stack>
+	);
+};
+
+export default OrderDetails;
--- components/Orders/Order.jsx
+++ components/Orders/Order.jsx
@@ -1,20 +1,28 @@
 import React from "react";
+import { TouchableOpacity } from "react-native";
 import { Image, View, Box, Text } from "native-base";
+import { useNavigation } from "@react-navigation/native";
+
+import screens from "~/constants/screens";
 import orderThumbnail from "~/assets/order_thumbnail.png";
 import styles from "./styles";
 
-const Order = ({status, date, orderID}) => {
+const Order = ({status, date, orderId}) => {
+	const navigation = useNavigation();
+	const { navigate } = navigation;
 	return (
-		<View style={styles.orderContainer} p="2"  borderRadius="md" borderWidth="1" borderColor="dark.50">
-			<Image source={orderThumbnail} alt="order thumbnail" />
-			<View w="1/3">
-				<Text fontSize="13" fontWeight="bold" >Order: {orderID}</Text>
-				<Text fontSize="12">{date}</Text>
+		<TouchableOpacity onPress={() => navigate({name: screens.ORDER_DETAILS, params: { orderId }})}>
+			<View style={styles.orderContainer} p="2"  borderRadius="md" borderWidth="1" borderColor="dark.50">
+				<Image source={orderThumbnail} alt="order thumbnail" />
+				<View w="1/3">
+					<Text fontSize="13" fontWeight="bold" >Order: {orderId}</Text>
+					<Text fontSize="12">{date}</Text>
+				</View>
+				<Box bgColor="dark.50" p={1.5} w="1/3" borderRadius="full">
+					<Text fontSize={12} color="white" textAlign="center">{status}</Text>
+				</Box>
 			</View>
-			<Box bgColor="dark.50" p={1.5} w="1/3" borderRadius="full">
-				<Text fontSize={12} color="white" textAlign="center">{status}</Text>
-			</Box>
-		</View>
+		</TouchableOpacity>
 	);
 };
 
