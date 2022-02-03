7a4c94cf83c1965e5ccf839e2dd026331a232eac
Mario Meraz <mario.meraz@gmail.com>
2022-02-03 16:38:17 -0600

    Avatar Style in Drawer
    add Avatar Style in Drawer

--- components/Navigation/DrawerNavigator.js
+++ components/Navigation/DrawerNavigator.js
@@ -13,6 +13,8 @@ import {
 	HStack,
 	Divider,
 	Icon,
+	Avatar,
+	IconButton
 } from "native-base";
 
 import Home from "~/components/Home";
@@ -43,48 +45,89 @@ function CustomDrawerContent(props) {
 		<DrawerContentScrollView {...props} safeArea>
 			<VStack space="6" my="2" mx="1">
 				<Box px="4">
-					<Text bold color="gray.700">
-            Mail
-					</Text>
-					<Text fontSize="14" mt="1" color="gray.500" fontWeight="500">
-            john_doe@gmail.com
+					<Avatar
+						bg="indigo.500"
+						size="xl"
+						source={{
+							uri: "https://images.unsplash.com/photo-1614289371518-722f2615943d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+						}}
+					></Avatar>
+					<IconButton
+						w={"40px"}
+						top={"60px"}
+						left={"70px"}
+						position={"absolute"}
+						icon={<Icon as={MaterialCommunityIcons} name="pencil" />}
+						borderRadius="full"
+						onPress={() => props.navigation.navigate("Profile")}
+						_icon={{
+							color: "red.500",
+							size: "sm",
+						}}
+						_hover={{
+							bg: "red.600:alpha.30",
+						}}
+						_pressed={{
+							bg: "red.600:alpha.30",
+							_icon: {
+								name: "pencil-outline",
+							},
+							_ios: {
+								_icon: {
+									size: "sm",
+								},
+							},
+						}}
+						_ios={{
+							_icon: {
+								size: "sm",
+							},
+						}}
+					/>
+
+					<Text bold color="primary.700" pt={5}>
+            Jonh Appleseed
 					</Text>
 				</Box>
+				<Divider />
 				<VStack divider={<Divider />} space="4">
 					<VStack space="3">
 						{props.state.routeNames.map((name, index) => (
-							<Pressable
-								px="5"
-								py="3"
-								rounded="md"
-								key={index}
-								bg={
-									index === props.state.index
-										? "rgba(6, 182, 212, 0.1)"
-										: "transparent"
-								}
-								onPress={() => {
-									props.navigation.navigate(name);
-								}}
-							>
-								<HStack space="7" alignItems="center">
-									<Icon
-										color={
-											index === props.state.index ? "primary.500" : "gray.500"
-										}
-										size="7"
-										as={<MaterialCommunityIcons name={getIcon(name)} />}
-									/>
-									<Text
-										fontWeight="500"
-										color={
-											index === props.state.index ? "primary.500" : "gray.700"
-										}
-									>
-										{name}
-									</Text>
-								</HStack>
-							</Pressable>
+							<>
+								<Pressable
+									px="5"
+									py="3"
+									rounded="md"
+									key={index}
+									bg={
+										index === props.state.index
+											? "rgba(6, 182, 212, 0.1)"
+											: "transparent"
+									}
+									onPress={() => {
+										props.navigation.navigate(name);
+									}}
+								>
+									<HStack space="7" alignItems="center">
+										<Icon
+											color={
+												index === props.state.index ? "primary.500" : "gray.500"
+											}
+											size="7"
+											as={<MaterialCommunityIcons name={getIcon(name)} />}
+										/>
+										<Text
+											fontWeight="500"
+											color={
+												index === props.state.index ? "primary.500" : "gray.700"
+											}
+										>
+											{name}
+										</Text>
+									</HStack>
+								</Pressable>
+								{name === "Help" && <Divider />}
+							</>
 						))}
 					</VStack>
 				</VStack>
