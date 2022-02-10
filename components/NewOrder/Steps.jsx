127dfc3b350bdde0701ecc834e2e1cefa8f9ca0e
eovando-93 <emmanuel.ovando@si-quimera.com.mx>
2022-02-10 14:46:02 -0600

    step with data

--- components/NewOrder/Steps.jsx
+++ components/NewOrder/Steps.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useRoute } from "@react-navigation/native";
-import { Text } from "native-base";
+import { Heading, Text, Center, Box } from "native-base";
 import { getStep } from "./helpers";
 import { useNewOrderContext } from "~/context/newOrder";
 
@@ -8,15 +8,60 @@ const Steps = () => {
 	const { totalSteps } = useNewOrderContext();
 	const { name } = useRoute();
 	const [currentStep, setCurrentStep] = useState(getStep(name));
+	const [info, setInfo] = useState({title: "", description: ""});
 
 
 	useEffect(() => {
 		setCurrentStep(getStep(name));
 	}, [name]);
 
+	useEffect(() => {
+		let data = {};
+		switch (currentStep.step) {
+		case 1: {
+			data = {
+				title: "Credit card picture",
+				description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elementum consectetur amet tellus lobortis diam sed."
+			};
+			break;
+		}
+		case 2: {
+			data = {
+				title: "Gravestone picture",
+				description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elementum consectetur amet tellus lobortis diam sed."
+			};
+			break;
+		}
+		case 3: {
+			data = {
+				title: "Order details",
+				description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elementum consectetur amet tellus lobortis diam sed."
+			};
+			break;
+		}
+		case 4: {
+			data = {
+				title: "Confirm order",
+				description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elementum consectetur amet tellus lobortis diam sed."
+			};
+			break;
+		}
+
+		default: data = {};
+		}
+		setInfo(data);
+
+	}, [currentStep]);
+
 
 	return (
-		<Text color={currentStep.color}>{currentStep.step} / {totalSteps}</Text>
+		<Center>
+			<Box>
+				<Text color={currentStep.color}>{currentStep.step} / {totalSteps}</Text>
+				<Heading color={currentStep.color}>{info.title}</Heading>
+				<Text color={currentStep.color}>{info.description}</Text>
+			</Box>
+		</Center>
 	);
 };
 
