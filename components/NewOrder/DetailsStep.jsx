7e46715cb8696aefe34f44cd60b8689bff2f6b7b
eovando-93 <emmanuel.ovando@si-quimera.com.mx>
2022-02-11 12:16:19 -0600

    Details step

--- components/NewOrder/DetailsStep.jsx
+++ components/NewOrder/DetailsStep.jsx
@@ -1,20 +1,117 @@
 import React from "react";
-import { Box, Center, Text, Button } from "native-base";
-import Steps from "./Steps";
+import { useForm, Controller } from "react-hook-form";
+import { TouchableOpacity } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import { 
+	Box,
+	VStack,
+	Input,
+	Button,
+	FormControl,
+	TextArea,
+	Text
+} from "native-base";
+import  Steps from "./Steps";
 import screens from "~/constants/screens";
 
 
-const DetailsStep = ({ navigation }) => {
-	const { navigate } = navigation;
+const DetailsStep = () => {
 	return (
-		<Center>
-			<Box alignItems="center">
-				<Steps />
-				<Text> Details step </Text>
-				<Button onPress={() => navigate(screens.NEW_ORDER_STEP_4)} >Next</Button>
-			</Box>
-		</Center>
+		<Box alignItems="center">
+			<Steps />
+			<Form />
+		</Box>
 	);
 };
+
+
+const Location = () => {
+	return (
+		<TouchableOpacity onPress={() => console.log("navigate(mAP)")}>
+			<Text mr="2" fontSize="11" underline>Locate in the map</Text>
+		</TouchableOpacity>
+	);
+};
+
+
+const Form = () => {
+	const { control, handleSubmit, formState: { errors } } = useForm();
+	const { navigate } = useNavigation();
+	const onSubmit = (data) => {
+		console.log({data});
+		navigate(screens.NEW_ORDER_STEP_4);
+	};
+	return (
+		<VStack w="full" maxW={280} mt="30" space={3} alignItems="center">
+			<FormControl isRequired isInvalid={"gravestoneText" in errors}>
+				<FormControl.Label>Gravestone text</FormControl.Label>
+				<Controller
+					control={control}
+					render={({ field: { onChange, onBlur, value } }) => (
+						<Input
+							variant="outline"
+							onBlur={onBlur}
+							onChangeText={(text) => onChange(text)}
+							value={value}
+						/>
+					)}
+					name="gravestoneText"
+					rules={{ required: "Field is required", minLength: 3 }}
+					defaultValue=""
+				/>
+				<FormControl.ErrorMessage>
+					{errors?.gravestoneText?.message}
+				</FormControl.ErrorMessage>
+			</FormControl>
+			<FormControl isRequired isInvalid={"additionalInformation" in errors}>
+				<FormControl.Label>Additional information</FormControl.Label>
+				<Controller
+					control={control}
+					render={({ field: { onChange, onBlur, value } }) => (
+						<TextArea
+							variant="outline"
+							onBlur={onBlur}
+							onChangeText={(text) => onChange(text)}
+							value={value}
+						/>
+					)}
+					name="additionalInformation"
+					rules={{ required: "Field is required", minLength: 3 }}
+					defaultValue=""
+				/>
+				<FormControl.ErrorMessage>
+					{errors?.additionalInformation?.message}
+				</FormControl.ErrorMessage>
+			</FormControl>
+			<FormControl isRequired isInvalid={"address" in errors}>
+				<FormControl.Label>Address</FormControl.Label>
+				<Controller
+					control={control}
+					render={({ field: { onChange, onBlur, value } }) => (
+						<Input
+							InputRightElement={<Location />}
+							variant="outline"
+							onBlur={onBlur}
+							onChangeText={(text) => onChange(text)}
+							value={value} 
+						/>
+					)}
+					name="address"
+					rules={{ required: "Field is required", minLength: 3 }}
+					defaultValue=""
+				/>
+				<FormControl.ErrorMessage>
+					{errors?.address?.message}
+				</FormControl.ErrorMessage>
+			</FormControl>
+			<Button borderRadius="none" bgColor="dark.50" onPress={handleSubmit(onSubmit)} size="lg" style={{width: "100%"}}>
+          Continue
+			</Button>
+		</VStack> 
+	);
+};
+
+
+
 
 export default DetailsStep;
