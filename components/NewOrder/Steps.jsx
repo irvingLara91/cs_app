61559ac7f8b1980600b6bb808f16058d0191fdee
eovando-93 <emmanuel.ovando@si-quimera.com.mx>
2022-02-11 10:03:50 -0600

    getStepData now returns all needed on Steps component

--- components/NewOrder/Steps.jsx
+++ components/NewOrder/Steps.jsx
@@ -1,66 +1,32 @@
 import React, { useEffect, useState } from "react";
-import { useRoute } from "@react-navigation/native";
-import { Heading, Text, Center, Box } from "native-base";
-import { getStep } from "./helpers";
+import { TouchableHighlight } from "react-native";
+import { useRoute, useNavigation } from "@react-navigation/native";
+import { Heading, Text, Center } from "native-base";
+
+import { getStepData } from "./helpers";
 import { useNewOrderContext } from "~/context/newOrder";
 
 const Steps = () => {
+	const { navigate } = useNavigation();
 	const { totalSteps } = useNewOrderContext();
 	const { name } = useRoute();
-	const [currentStep, setCurrentStep] = useState(getStep(name));
-	const [info, setInfo] = useState({title: "", description: ""});
-
+	const [currentStep, setCurrentStep] = useState(getStepData(name));
 
 	useEffect(() => {
-		setCurrentStep(getStep(name));
+		setCurrentStep(getStepData(name));
 	}, [name]);
-
-	useEffect(() => {
-		let data = {};
-		switch (currentStep.step) {
-		case 1: {
-			data = {
-				title: "Credit card picture",
-				description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elementum consectetur amet tellus lobortis diam sed."
-			};
-			break;
-		}
-		case 2: {
-			data = {
-				title: "Gravestone picture",
-				description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elementum consectetur amet tellus lobortis diam sed."
-			};
-			break;
-		}
-		case 3: {
-			data = {
-				title: "Order details",
-				description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elementum consectetur amet tellus lobortis diam sed."
-			};
-			break;
-		}
-		case 4: {
-			data = {
-				title: "Confirm order",
-				description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elementum consectetur amet tellus lobortis diam sed."
-			};
-			break;
-		}
-
-		default: data = {};
-		}
-		setInfo(data);
-
-	}, [currentStep]);
 
 
 	return (
-		<Center>
-			<Box>
-				<Text color={currentStep.color}>{currentStep.step} / {totalSteps}</Text>
-				<Heading color={currentStep.color}>{info.title}</Heading>
-				<Text color={currentStep.color}>{info.description}</Text>
-			</Box>
+		<Center pt="10">
+			<Center maxW="250">
+				<Text color={currentStep.color} fontSize="20" >{currentStep.step} / {totalSteps}</Text>
+				<Heading color={currentStep.color} fontWeight="bold" fontSize="18" >{currentStep.data.title}</Heading>
+				<Text color={currentStep.color} fontSize="10">{currentStep.data.description}</Text>
+				<TouchableHighlight onPress={() => console.log("navigate(currentStep.data.help)")}>
+					<Text mt="3" fontSize="11" color={currentStep.color} underline>Need help?</Text>
+				</TouchableHighlight>
+			</Center>
 		</Center>
 	);
 };
