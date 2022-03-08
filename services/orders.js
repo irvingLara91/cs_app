341a49282c37675ec4fa8e84298b7b85097a8851
Irving Lara <irving.lara@si-quimera.com.mx>
2022-03-08 13:03:33 -0600

    Create firebase API for orders.

--- services/orders.js
+++ services/orders.js
@@ -1,28 +1,40 @@
-import { FAKE_ORDER_DETAIL, ORDERS_FAKE_DATA } from "~/utils";
+import {FAKE_ORDER_DETAIL, ORDERS_FAKE_DATA} from "~/utils";
+import {auth, db, avatarStorageRef} from "~/firebase";
+import {collection, doc, getDoc, query, where, getDocs} from "firebase/firestore";
+
+const getOrdersAll = async (userId) => {
+    const orders = [];
+    const query_ = query(collection(db, "orders"));
+    const querySnapshot = await getDocs(query_);
+    querySnapshot.forEach((doc) => {
+        orders.push(doc.data())
+    });
+    return orders
+}
 
 const getOrders = (userId) => {
-	// FAKE API CALL
-	return new Promise((resolve, reject) => {
-		const orders = ORDERS_FAKE_DATA.map((o) => o);
-		try {
-			resolve(orders);
-		} catch(error) {
-			reject(error);
-		}
-	});
+    // FAKE API CALL
+    return new Promise((resolve, reject) => {
+        const orders = ORDERS_FAKE_DATA.map((o) => o);
+        try {
+            resolve(orders);
+        } catch (error) {
+            reject(error);
+        }
+    });
 };
 
 const getOrderDetails = (orderId) => {
-	return new Promise((resolve, reject) => {
-		const details = FAKE_ORDER_DETAIL;
-		try {
-			resolve(details);
-		} catch(error) {
-			reject(error);
-		}
-	});
+    return new Promise((resolve, reject) => {
+        const details = FAKE_ORDER_DETAIL;
+        try {
+            resolve(details);
+        } catch (error) {
+            reject(error);
+        }
+    });
 };
 
-const ordersService = { getOrders, getOrderDetails };
+const ordersService = {getOrders, getOrderDetails, getOrdersAll};
 
 export default ordersService;
