656c7ab612a0a80b49c7e8931d4567910ee30804
Irving Lara <irving.lara@si-quimera.com.mx>
2022-03-23 15:20:08 -0600

    Design list orders

--- components/common/Order.jsx
+++ components/common/Order.jsx
@@ -1,21 +1,34 @@
 import React from "react";
-import {TouchableOpacity, StyleSheet} from "react-native";
-import {Image, View, Box, Text} from "native-base";
+import {TouchableOpacity,View, StyleSheet} from "react-native";
+import {Image, Box, Text} from "native-base";
 import {useNavigation} from "@react-navigation/native";
 
 import screens from "~/constants/screens";
-import {statusCode} from "~/utils/utils";
-import orderThumbnail from "~/assets/order_thumbnail.png";
+import {SCREEN_WIDTH, statusCode, textSizeRender} from "~/utils/utils";
 import moment from "moment";
+import {MaterialIcons} from "@expo/vector-icons";
 
 
 const styles = StyleSheet.create({
     orderContainer: {
+        borderWidth:1,
+        marginBottom:15,
+        padding:8,
+        borderRadius:10,
+        borderColor:"#C4C4C4",
         display: "flex",
         flexDirection: "row",
         justifyContent: "space-between",
         width: "100%",
-        alignItems: "center"
+        alignItems: "center",
+        backgroundColor: 'white',
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0, height: 4,
+        },
+        shadowOpacity: 0.30,
+        shadowRadius: 4.65,
+        elevation: 8,
     },
 });
 
@@ -23,23 +36,36 @@ const Order = ({status, orderId, card, ...props}) => {
     const navigation = useNavigation();
     const {navigate} = navigation;
     return (
-        <TouchableOpacity onPress={() => navigate({name: screens.ORDER_DETAILS, params: {orderId}})}>
-            <View style={styles.orderContainer} p="2" borderRadius="md" borderWidth="1" borderColor="dark.50">
-                <Image size={"xs"} source={{uri: card}} alt="order thumbnail"/>
-                <View w="1/3">
-                    <Text fontSize="13" fontWeight="bold">Order: {orderId}</Text>
-                    <Text fontSize="12">
+        <TouchableOpacity
+            style={styles.orderContainer}
+            onPress={() => navigate({name: screens.ORDER_DETAILS, params: {orderId}})}>
+                <Image
+                    flex={0}
+                    size={SCREEN_WIDTH*.15} source={{uri: card}}  borderRadius={10} alt="order thumbnail"/>
+                <View style={{flex:1.5,marginLeft:8}}>
+                    <Text fontSize={textSizeRender(3.2)} fontFamily={"Roboto_700Bold"}>Order: {orderId}</Text>
+                    <Text fontSize={textSizeRender(3.2)} fontFamily={"Roboto_400Regular"}>
                         {
-                            props.timestamp ? moment(props.timestamp.seconds * 1000, "", "en").format('MM/DD/YYYY')
+                            props.createdAt ? moment(props.createdAt.seconds * 1000, "", "en").format('MM/DD/YYYY')
                                 :
-                                "No date"
+
+                                props.timestamp ? moment(props.timestamp.seconds * 1000, "", "en").format('MM/DD/YYYY')
+                                    :
+                                    "No date"
                         }
                     </Text>
-
                 </View>
-                <Box bgColor="dark.50" p={1.5} w="1/3" borderRadius="full">
+                <View style={{
+                    flex:1,
+                    width:SCREEN_WIDTH*.2,backgroundColor: 'black',padding: 5,borderRadius: 20}}>
                     <Text fontSize={12} color="white" textAlign="center">{statusCode(props.statusCode)}</Text>
-                </Box>
+                </View>
+            <View style={{
+                justifyContent: "center",
+                alignItems: "center",
+                flex:.3}}>
+                <MaterialIcons name="arrow-forward-ios" size={24} color="black" />
+
             </View>
         </TouchableOpacity>
     );
